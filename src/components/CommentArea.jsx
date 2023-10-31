0aab3006cfef96e01c18e26f044445240d4c83cf
Riccardo Di Bari <riccardodibari@hotmail.it>
2023-10-31 16:53:59 +0100

    ebibook terzo gg

---
+++ src/components/CommentArea.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
+
+const CommentArea = (props) => {
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      setIsLoading(true);
+      try {
+        let response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/comments/" + props.asin,
+          {
+            headers: {
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTNmZTVhNmIzOTczNDAwMTRkNWU5MDIiLCJpYXQiOjE2OTg2ODYzNzQsImV4cCI6MTY5OTg5NTk3NH0.MD6Rv_alxOyxFTyogijyZ1yibUWJivSuHbWzZ2XPFU4" 
+
+            },
+          }
+        );
+        console.log(response);
+        if (response.ok) {
+          let comments = await response.json();
+          setComments(comments);
+          setIsLoading(false);
+          setIsError(false);
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+        }
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setIsError(true);
+      }
+    };
+
+    if (props.asin) {
+      fetchComments();
+    }
+  }, [props.asin]);
+
+  return (
+    <div className="text-center">
+      {isLoading && <Loading />}
+      {isError && <Error />}
+      <AddComment asin={props.asin} />
+      <CommentList commentsToShow={comments} />
+    </div>
+  );
+};
+
+export default CommentArea;
---
+++ src/components/SingleBook.jsx
@@ -0,0 +1,27 @@
+import { Card } from "react-bootstrap";
+
+
+const SingleBook = (props) => {
+ 
+
+  return (
+    <>
+      <Card
+     
+        onClick={() => props.changeSelectedBook(props.book.asin)}
+        style={{
+          border:
+            props.selectedBook === props.book.asin ? "3px solid red" : "none",
+        }}
+      >
+        <Card.Img variant="top" src={props.book.img} />
+        <Card.Body>
+          <Card.Title style={{ color: "black" }}>{props.book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+      {/* {this.state.selected && <CommentArea asin={this.props.book.asin} />} */}
+    </>
+  );
+};
+
+export default SingleBook;
